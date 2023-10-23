9d7c76baaa738c51e19c845c3edfea4766c6e104
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2023-10-22 23:21:41 -0700

    initial

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./graphql/schema/schema');
+
+
+const itemResolver = require('./graphql/resolver/ItemResolver');
+const customerResolver = require('./graphql/resolver/CustomerResolver');
+const vendorResolver = require('./graphql/resolver/VendorResolver');
+
+
+const resolvers = { 
+  Query: {
+    ...itemResolver.Query,
+    ...customerResolver.Query,
+    ...vendorResolver.Query,
+  },
+  Mutation: {
+    ...itemResolver.Mutation,
+    ...customerResolver.Mutation,
+    ...vendorResolver.Mutation,
+  },
+};
+
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // rootValue: db,
+  // tracing: true
+
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server running at ${url}`);
+});
